d8b782001fde6475f015165737a6bba387df914e
GameDog9988 <alex@grattan.me>
2021-10-25 13:52:29 -0400

    feat: start to queries

---
+++ helpers/queries.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const polygonApiKey = process.env.NEXT_PUBLIC_POLYGON_API_KEY;
+const ameritradeApiKey = process.env.NEXT_PUBLIC_TD_AMERITRADE_API_KEY;
+
+export const fetchAllTickers = async () => {
+  const res = await axios.get(
+    `https://api.polygon.io/v3/reference/tickers?type=CS&market=stocks&active=true&sort=ticker&order=asc&limit=1000&apiKey=${polygonApiKey}`
+  );
+  return res;
+};
+
+export const fetchTicker = async () => {
+  const res = await axios.get(
+    `https://api.polygon.io/v3/reference/tickers?active=true&sort=ticker&order=asc&limit=10&apiKey=${polygonApiKey}`
+  );
+  return res;
+};
+
+export const searchTicker = async (search) => {
+  const res = await axios.get(
+    `https://api.polygon.io/v3/reference/tickers?search=${search}&active=true&sort=ticker&order=asc&limit=10&apiKey=${polygonApiKey}`
+  );
+  return res;
+};
+
+export const fetchPriceHistory = async () => {
+  const res = await axios.get(
+    `https://api.tdameritrade.com/v1/marketdata/AAPL/pricehistory?apikey=${ameritradeApiKey}`
+  );
+  return res;
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 import GlobalStyles from "./../components/GlobalStyles";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function MyApp({ Component, pageProps }) {
+  const queryClient = new QueryClient();
+
   return (
     <div>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </div>
   );
 }
