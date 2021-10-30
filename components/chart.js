40aa769c4c787cfd38114eb1857076372a66b778
GameDog9988 <alex@grattan.me>
2021-10-29 23:43:12 -0400

    feat: styled chart

--- components/chart.js
+++ components/chart.js
@@ -1,21 +1,34 @@
-import { Line } from "react-chartjs-2";
+import { Line, defaults } from "react-chartjs-2";
 import "chartjs-adapter-date-fns";
 import "twin.macro";
 
 const Chart = ({ chartData }) => {
+  defaults.font.family = "Montserrat";
+
   // Chart.js data
-  const data = {
-    datasets: [
-      {
-        label: "Portfolio Value",
-        data: chartData,
-      },
-    ],
+  const data = (canvas) => {
+    const ctx = canvas.getContext("2d");
+    const g = ctx.createLinearGradient(0, 0, 0, 400);
+    g.addColorStop(0, "#44B4FF");
+    g.addColorStop(1, "#99D6FF");
+    return {
+      datasets: [
+        {
+          label: "Portfolio Value",
+          data: chartData,
+          backgroundColor: g,
+          fill: true,
+          // borderColor: "#fff",
+          pointBackgroundColor: "#9D8DF1",
+        },
+      ],
+    };
   };
 
   // Chart.js options
   const options = {
     responsive: true,
+    radius: 4,
     parsing: {
       xAxisKey: "datetime",
       yAxisKey: "close",
@@ -23,6 +36,9 @@ const Chart = ({ chartData }) => {
     scales: {
       x: {
         type: "time",
+        font: {
+          family: "Montserrat",
+        },
       },
       y: {
         ticks: {
